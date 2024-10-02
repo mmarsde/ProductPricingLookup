ae5f84ff4a766274ffef22608ec236a4e993c238
Michelle Marsden <mmarsd060184@gmail.com>
2024-10-02 11:03:14 +0100

    Refactored ProductPricingService to check the existence of products in a products collection. Fixed failing unit tests.

--- tests/ProductPricing.Application.UnitTests/Services/ProductPricingServiceTests.cs
+++ tests/ProductPricing.Application.UnitTests/Services/ProductPricingServiceTests.cs
@@ -1,3 +1,5 @@
+using ProductPricing.Application.Models.Domain;
+
 namespace ProductPricing.Application.UnitTests.Services;
 
 public class ProductPricingServiceTests
@@ -6,9 +8,11 @@ public class ProductPricingServiceTests
         
     [Theory]
     [InlineData(1, true)]
-    [InlineData(2, false)]
+    [InlineData(2, true)]
     [InlineData(3, true)]
     [InlineData(4, false)]
+    [InlineData(5, true)]
+    [InlineData(6, false)]    
     public void CheckProductExists_ShouldReturnExpectedResult(int id, bool expectedResult)
     {
         //act
@@ -21,9 +25,41 @@ public class ProductPricingServiceTests
 
 internal class ProductPricingService : IProductPricingService
 {
+    private readonly IEnumerable<ProductPricingModel> _productPrices = new []
+    {
+        new ProductPricingModel
+        {
+            Id = 1,
+            Name = "Product A",
+            CurrentPrice = 100.0M,
+            LastUpdated = DateTime.UtcNow,
+        },
+        new ProductPricingModel
+        {
+            Id = 2,
+            Name = "Product B",
+            CurrentPrice = 110.0M,
+            LastUpdated = DateTime.UtcNow,
+        },
+        new ProductPricingModel
+        {
+            Id = 3,
+            Name = "Product C",
+            CurrentPrice = 120.0M,
+            LastUpdated = DateTime.UtcNow,
+        },
+        new ProductPricingModel
+        {
+            Id = 5,
+            Name = "Product E",
+            CurrentPrice = 130.0M,
+            LastUpdated = DateTime.UtcNow,
+        },         
+    };
+    
     public bool CheckProductExists(int id)
     {
-        return id == 1;
+        return _productPrices.Any(p => p.Id == id);
     }
 }
 
